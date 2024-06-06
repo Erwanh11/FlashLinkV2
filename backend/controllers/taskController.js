12235d073b587be9a95a4d956675135027b24b1c
Erwanh11 <95217491+Erwanh11@users.noreply.github.com>
2024-06-06 11:20:26 -0400

    Notificationmailer

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,3 +1,4 @@
+const nodemailer = require('nodemailer');
 const Task = require('../models/Task');
 exports.getTasks = async (req, res) => {
   try {
@@ -7,11 +8,31 @@ exports.getTasks = async (req, res) => {
     res.status(500).json({ message: 'Server error' });
   }
 };
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 exports.createTask = async (req, res) => {
   const { title, description, priority } = req.body;
   try {
     const task = new Task({ user: req.user.id, title, description, priority });
     await task.save();
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: req.user.email,
+      subject: 'New Task Created',
+      text: `Task ${title} has been created with priority ${priority}.`,
+    };
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
     res.status(201).json(task);
   } catch (error) {
     res.status(500).json({ message: 'Server error' });
